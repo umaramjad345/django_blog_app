5791dc55664f319ac21bc638b3e548db29bfde1e
umaramjad345 <umarziaii345@gmail.com>
2024-06-07 01:03:47 +0500

    making and styling header and footer files

--- BlogApp/settings.py
+++ BlogApp/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
